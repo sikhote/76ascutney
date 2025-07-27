dfeba82c8bc4eec148ef2b9d979bccbb01d5ee57
David Sinclair <david_sinclair@apple.com>
2025-07-27 13:23:03 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 import { promises as fs } from 'fs';
 import Md from 'components/Md';
-import Image from 'next/image';
-import styles from './styles.module.scss';
 
 export default async function Page() {
   const source = await fs.readFile('public/content/home.md', 'utf8');
